38fb87a695a0e14c4ae674036fd744f939942f9d
Diyorbektuit <d.xazratqulov@tuit.uz>
2025-01-25 22:25:07 +0500

    admin

--- main/admin.py
+++ main/admin.py
@@ -9,37 +9,51 @@ admin.site.unregister(User)
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name_uz', 'name_eng', 'name_ru', 'order')
+    search_fields = ('name_uz', 'name_eng', 'name_ru')
 
 
 @admin.register(models.Company)
 class CompanyAdmin(admin.ModelAdmin):
     list_display = ('id', 'name_uz', 'name_eng', 'name_ru')
+    search_fields = ('name_uz', 'name_eng', 'name_ru')
 
 
 @admin.register(models.Country)
 class CountryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name_uz', 'name_eng', 'name_ru')
+    search_fields = ('name_uz', 'name_eng', 'name_ru')
+
+
+class ApplicationInline(admin.TabularInline):
+    model = models.Application
+    extra = 0
+    fields = ('fullname', 'phone_number', 'email', 'extra_description')
+    readonly_fields = ('fullname', 'phone_number', 'email', 'extra_description')
 
 
 @admin.register(models.Vacancy)
-class CountryAdmin(admin.ModelAdmin):
+class VacancyAdmin(admin.ModelAdmin):
     list_display = ('id', 'title_eng', 'company', 'category', 'country')
-    list_filter = ('job_schedule', 'work_location')
+    list_filter = ('job_schedule', 'work_location', 'category', 'country', 'company')
+    search_fields = ('title_uz', 'title_eng', 'title_ru')
+    inlines = (ApplicationInline, )
 
 
 @admin.register(models.Application)
-class CountryAdmin(admin.ModelAdmin):
+class ApplicationAdmin(admin.ModelAdmin):
     list_display = ('id', 'fullname', 'phone_number', 'email', 'country', 'vacancy')
     list_filter = ('country', 'vacancy')
+    search_fields = ('fullname', 'phone_number', 'email')
 
 
 @admin.register(models.Post)
-class CountryAdmin(admin.ModelAdmin):
+class PostAdmin(admin.ModelAdmin):
     list_display = ('id', 'title_eng', 'poster', 'slug')
     readonly_fields = ('slug', )
+    search_fields = ('title_uz', 'title_eng', 'title_ru')
 
 
 @admin.register(models.Form)
-class CountryAdmin(admin.ModelAdmin):
+class FormAdmin(admin.ModelAdmin):
     list_display = ('id', 'fullname', 'phone_number', 'description')
     search_fields = ('fullname', 'phone_number')
