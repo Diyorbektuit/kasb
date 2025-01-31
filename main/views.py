7ee8616d15073cbc6e5b0758b83b5531b34631f7
Diyorbektuit <d.xazratqulov@tuit.uz>
2025-01-31 18:03:11 +0500

    admin

--- main/views.py
+++ main/views.py
@@ -205,6 +205,18 @@ class CompanyListView(generics.ListAPIView):
     queryset = Company.objects.all()
     serializer_class = serializer.CompanyListSerializer
 
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                'language',
+                openapi.IN_QUERY,
+                description="language codeni yozasiz",
+                type=openapi.TYPE_STRING
+            )
+        ])
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
     def get_serializer_context(self):
         context = super().get_serializer_context()
         context["language"] = self.request.query_params.get("language", None)
@@ -213,6 +225,18 @@ class CompanyListView(generics.ListAPIView):
 class CategoryListView(generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = serializer.CategoryListSerializer
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                'language',
+                openapi.IN_QUERY,
+                description="language codeni yozasiz",
+                type=openapi.TYPE_STRING
+            )
+        ])
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
